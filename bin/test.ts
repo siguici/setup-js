bb8a675b24efe3a2572eb34133f2e60016c80933
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-10 07:08:53 +0100

    :green_heart: Ensure that the action works properly

---
+++ bin/test.ts
@@ -0,0 +1,10 @@
+import { assert } from '@japa/assert';
+import { configure, processCLIArgs, run } from '@japa/runner';
+
+processCLIArgs(process.argv.splice(2));
+configure({
+  files: ['tests/**/*.spec.ts'],
+  plugins: [assert()]
+});
+
+run();
