4ea57174b2724e178d000dbebbc5e246311a2cb5
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-11 07:17:45 +0100

    :safety_vest: Make sure the script is defined and not empty

--- scripts/messages.sh
+++ scripts/messages.sh
@@ -11,8 +11,18 @@ declare -A script_messages=(
   ["release"]="📦 Project release"
 )
 
+isset() {
+  if [ -z "${1:-}" ]; then
+    echo "Undefined or empty variable" >&2
+    return 1
+  fi
+}
+
 get_script_message() {
   local script="$1"
+  if ! isset "$script"; then
+    return 1
+  fi
   for key in "${!script_messages[@]}"; do
     if [[ "$script" == $key || "$script" == $key:* || "$script" == $key.* || "$script" == *:$key || "$script" == *.$key ]]; then
       echo "${script_messages[$key]} ($script)"
