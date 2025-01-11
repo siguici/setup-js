65b43f91d0ded6b3cc5af3da692c5e8c57f46388
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-11 19:15:13 +0100

    :arrow_up: Update Bash on setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Running on macOS..."
+  if ((BASH_VERSINFO[0] < 4)); then
+    echo "⬆️Update Bash..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install bash
+    echo "Bash version : $(bash --version)"
+  fi
+else
+  echo "No Bash update required for $OSTYPE"
+fi
+
 set -euo pipefail
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
