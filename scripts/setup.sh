e214b95bc1c098cf90d1b31021567739406fcefb
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-12 12:42:23 +0100

    :children_crossing: Improve detection of operating system information

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,7 @@ pm=${pm:-""}
 pm_version=${pm_version:-"latest"}
 pm_lockfile=${pm_lockfile:-"none"}
 os=${os:-""}
+os_name=${os_name:-""}
 os_version=${os_version:-""}
 os_arch=${os_arch:-""}
 
@@ -30,7 +31,7 @@ detect_runtime() {
   fi
 }
 
-detect_package_manager() {
+detect_pm() {
   if [ -n "$pm" ]; then
     read -r pm pm_version <<< "$(parse_pm "$pm")"
     valid_pms=("npm" "yarn" "pnpm" "bun" "deno")
@@ -59,54 +60,69 @@ detect_package_manager() {
   pm=${pm:-"npm"}
 }
 
+normalize_arch() {
+  case "$1" in
+    x86_64|amd64) os_arch="x64" ;;
+    i386|i686) os_arch="x86" ;;
+    armv7*|armhf) os_arch="arm" ;;
+    aarch64|arm64) os_arch="arm64" ;;
+    riscv64) os_arch="riscv64" ;;
+    *) os_arch="unknown" ;;
+  esac
+}
+
 detect_os() {
   if [ -z "$os" ]; then
     case "$OSTYPE" in
-      darwin*) os="macOS" ;;
-      linux-gnu*)
-        os="Linux"
-        os_version=$(grep '^VERSION_ID' /etc/os-release 2>/dev/null | cut -d '=' -f 2 | tr -d '"')
-      ;;
-      msys*|cygwin*|win32*) os="Windows" ;;
-      *) os="Unknown" ;;
+      darwin*) os="darwin" ;;
+      linux*) os="linux" ;;
+      msys*|cygwin*|win32*) os="windows" ;;
+      *) os="unknown" ;;
     esac
   fi
 
-  if [ -z "$os_version" ]; then
-    if [[ "$os" == "macOS" ]]; then
-      os_version=$(sw_vers -productVersion)
-    elif [[ "$os" == "Linux" ]]; then
-      os_version=$(lsb_release -rs 2>/dev/null || echo "$(uname -r)")
-    elif [[ "$os" == "Windows" ]]; then
-      os_version=$(powershell -Command "(Get-CimInstance -Class Win32_OperatingSystem).Version")
+  if [[ "$os" == "darwin" ]]; then
+    os_name="macOS"
+    os_version=$(sw_vers -productVersion)
+  elif [[ "$os" == "linux" ]]; then
+    if [ -f "/etc/os-release" ]; then
+      os_name=$(grep '^NAME=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+      os_version=$(grep '^VERSION_ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
     else
-      os_version="Unknown"
+      os_name="Generic"
+      os_version=$(lsb_release -rs 2>/dev/null || echo "$(uname -r)")
     fi
+  elif [[ "$os" == "windows" ]]; then
+    os_name="Windows"
+    os_version=$(powershell -Command "(Get-CimInstance -Class Win32_OperatingSystem).Version")
+  else
+    os_name="Unknown"
+    os_version="unknown"
   fi
 
-  os_arch=$(uname -m)
+  os_arch=$(normalize_arch $(uname -m))
 }
 
-os_info(){
+os_info() {
   case "$os" in
-    "macOS") OS="$MACOS" ;;
-    "Linux")
+    "darwin") OS="$DARWIN" ;;
+    "linux")
       if grep -qi microsoft /proc/version 2>/dev/null; then
         OS="$WINDOWS Subsystem for $LINUX (WSL)"
       else
         OS="$LINUX"
       fi
       ;;
-    "Windows") OS="$WINDOWS" ;;
+    "windows") OS="$WINDOWS" ;;
     *) OS="$UNKNOWN OS, type: $OSTYPE" ;;
   esac
 
-  echo -e "$OS ($os_version, $os_arch)"
+  echo -e "$OS ($os_name $os_version, $os_arch)"
 }
 
-detect_os
 detect_runtime
-detect_package_manager
+detect_pm
+detect_os
 
 if [[ "$pm" == "$runtime" ]]; then
   info "Using $runtime"
@@ -122,6 +138,7 @@ os_info
   echo "pm_version=$pm_version"
   echo "pm_lockfile=$pm_lockfile"
   echo "os=$os"
+  echo "os_name=$os_name"
   echo "os_version=$os_version"
   echo "os_arch=$os_arch"
 } >> "$GITHUB_ENV"
